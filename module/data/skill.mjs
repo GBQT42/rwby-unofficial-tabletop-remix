14536460ff4676ae35bb4b0ce5588602989556db
GBQT <nheutte@gmail.com>
2024-05-18 19:10:48 +0200

    Update skill default formula to include roll description

--- module/data/skill.mjs
+++ module/data/skill.mjs
@@ -8,7 +8,7 @@ export default class rwbySkill extends rwbyItemBase {
         const requiredInteger = { required: true, nullable: false, integer: true };
         const schema = super.defineSchema();
 
-        schema.rollModifier = new fields.StringField({ initial: "@abilities.dex.value + @abilities.dis.value" });
+        schema.rollModifier = new fields.StringField({ initial: "@abilities.dex.value[DEX] + @abilities.dis.value[DIS]" });
         schema.nativeDifficulty = new fields.NumberField({ ...requiredInteger, initial: 0 });
 
         return schema;
